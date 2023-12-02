325d6d1d768c30a2775086cad676af36c5bcfc83
edgardoraul <edgardo_raul@hotmail.com>
2023-12-02 01:51:59 -0300

    no fuciona...pero se va encaminado

--- las_galerias.php
+++ las_galerias.php
@@ -2,6 +2,15 @@
 // Asegúrate de cargar WordPress
 require_once('wp-load.php');
 
+// Carga el archivo media.php para que se pueda usar media_sideload_image()
+require_once ABSPATH . 'wp-admin/includes/media.php';
+
+// Ruta al archivo de registro
+$log_file = __DIR__ . '/metabox_migration_log.log';
+
+// Inicia el archivo de registro con un mensaje de inicio de ejecución
+log_message("Inicio del script");
+
 // Obtén todas las publicaciones de tipo 'post'
 $args = array(
     'post_type' => 'post',
@@ -20,22 +29,53 @@ if ($posts_query->have_posts()) {
 
         // Filtra elementos vacíos
         $thumbs = array_filter($thumbs);
-		var_dump($thumbs);
 
-        // Actualiza el campo personalizado 'galeria'
-        if (!empty($thumbs)) {
-            update_post_meta(get_the_ID(), 'galeria', $thumbs);
-			var_dump(galeria);
-            echo 'Actualizado galeria para la publicación ' . get_the_ID() . '<br>';
-        } else {
-            echo 'No hay datos para la publicación ' . get_the_ID() . '<br>';
+        // Procesa cada imagen en 'thumbs' y adjúntala al post
+        foreach ($thumbs as $image_url) {
+            // Usa la función wp_insert_attachment para adjuntar la imagen al post
+            $attachment_id = upload_image_from_url($image_url, get_the_ID());
+
+            // Agrega el attachment ID al nuevo metabox 'galeria'
+            if ($attachment_id) {
+                $galeria = get_post_meta(get_the_ID(), 'galeria', true);
+                $galeria[] = $attachment_id;
+                update_post_meta(get_the_ID(), 'galeria', $galeria);
+            } else {
+                // Registra un mensaje de error si la imagen no se pudo adjuntar
+                log_message("Error al adjuntar la imagen ($image_url) a la publicación ID: " . get_the_ID());
+            }
         }
     }
 
     wp_reset_postdata(); // Restablece los datos de la consulta
 
-    echo '¡Proceso completado!';
+    // Registra un mensaje de éxito al finalizar el script
+    log_message("¡Proceso completado!");
 } else {
-    echo 'No se encontraron publicaciones para actualizar.';
+    // Registra un mensaje si no se encuentran publicaciones para actualizar
+    log_message("No se encontraron publicaciones para actualizar.");
+}
+
+/**
+ * Función para adjuntar una imagen al post desde una URL
+ */
+function upload_image_from_url($image_url, $post_id) {
+    // Guarda la imagen en la biblioteca de medios y obtén el ID del archivo adjunto
+    $image_id = media_sideload_image($image_url, $post_id, '', 'id');
+
+    return $image_id;
+}
+
+/**
+ * Función para registrar mensajes en el archivo de registro
+ */
+function log_message($message) {
+    global $log_file;
+
+    // Formatea el mensaje con la fecha y hora
+    $formatted_message = "[" . date("Y-m-d H:i:s") . "] " . $message . "\n";
+
+    // Agrega el mensaje al archivo de registro
+    file_put_contents($log_file, $formatted_message, FILE_APPEND);
 }
 ?>
